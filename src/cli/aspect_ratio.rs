157a2a6c30b33ea6784f0c1f7cada0b1801865b0
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-08-17 18:12:11 +0700

    refactor(cli): moved functions to method

--- src/cli/aspect_ratio.rs
+++ src/cli/aspect_ratio.rs
@@ -1,11 +1,13 @@
 use structopt::StructOpt;
 
-#[derive(StructOpt, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(StructOpt, Debug, Clone, Copy)]
 pub enum AspectRatio {
     /// Enables aspect ratio check
     Enable,
     /// Disables aspect ratio check
     Disable,
+    /// Set aspect ratio range
+    Range { input: f32 },
     /// Set aspect ratio height
     Height { input: u32 },
     /// Set aspect ratio width
@@ -15,18 +17,21 @@ pub enum AspectRatio {
 impl AspectRatio {
     pub fn handle(&self, profile: &str) {
         match self {
-            &Self::Enable => enable(profile),
-            &Self::Disable => disable(profile),
-            &Self::Height { input } => height(input, profile),
-            &Self::Width { input } => width(input, profile),
+            Self::Enable => self.enable(profile),
+            Self::Disable => self.disable(profile),
+            &Self::Height { input } => self.height(input, profile),
+            &Self::Width { input } => self.width(input, profile),
+            &Self::Range { input } => self.range(input, profile),
         }
     }
+
+    fn enable(&self, profile: &str) {}
+
+    fn disable(&self, profile: &str) {}
+
+    fn height(&self, input: u32, profile: &str) {}
+
+    fn width(&self, input: u32, profile: &str) {}
+
+    fn range(&self, input: f32, profile: &str) {}
 }
-
-fn enable(profile: &str) {}
-
-fn disable(profile: &str) {}
-
-fn height(height: u32, profile: &str) {}
-
-fn width(width: u32, profile: &str) {}
