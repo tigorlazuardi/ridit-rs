94b5be168a2f89742d62ccfb81713b91640905eb
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-08-21 16:46:38 +0700

    WIP(config): added default implementations to config

--- src/api/config/model.rs
+++ src/api/config/model.rs
@@ -1,5 +1,6 @@
 use std::{collections::HashMap, path::PathBuf};
 
+use directories::UserDirs;
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug, Clone)]
@@ -18,11 +19,32 @@ pub struct AspectRatio {
 	pub range: f32,
 }
 
+impl AspectRatio {
+	pub fn default() -> Self {
+		AspectRatio {
+			enable: true,
+			height: 9,
+			width: 16,
+			range: 0.3,
+		}
+	}
+}
+
 #[derive(Debug, Deserialize, Clone, Copy)]
 pub struct MinimumSize {
 	pub enable: bool,
 	pub height: u32,
 	pub width: u32,
+}
+
+impl MinimumSize {
+	pub fn default() -> Self {
+		MinimumSize {
+			enable: true,
+			height: 1080,
+			width: 1920,
+		}
+	}
 }
 
 #[derive(Deserialize, Debug, Clone)]
@@ -31,11 +53,35 @@ pub struct Download {
 	pub connect_timeout: u32,
 }
 
+impl Download {
+	pub fn default() -> Self {
+		let dir = UserDirs::new().expect("failed to determine user directory");
+		let dir = dir
+			.picture_dir()
+			.expect("failed to get user picture directory")
+			.to_path_buf();
+		Download {
+			path: dir.join("ridit"),
+			connect_timeout: 10,
+		}
+	}
+}
+
 #[derive(Deserialize, Debug, Clone, Copy)]
 pub struct Subreddit {
 	pub nsfw: bool,
 	pub download_first: bool,
 	pub sort: Sort,
+}
+
+impl Subreddit {
+	pub fn default() -> Self {
+		Subreddit {
+			nsfw: true,
+			download_first: false,
+			sort: Sort::New,
+		}
+	}
 }
 
 #[derive(Deserialize, Debug, Clone, Copy)]
